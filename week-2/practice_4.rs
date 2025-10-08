5a6a764e3f25f9f3ec92209281e3217b5754da1f
matoeCOS101 <morgan.atoe@pau.edu.ng>
2025-10-08 17:32:48 +0100

    (My second commit)

---
+++ week-2/practice_4.rs
@@ -0,0 +1,11 @@
+fn main(){
+	Let p:f64 = 1000.0;
+	Let r:f64 = 1.0;
+	Let t:f64 = 2.0;
+
+	// simple interest
+	let a = p * ( 1.0 + (r / 100.0)) * t;
+	println!("Amount is {}", a);
+	let si = a - p;
+	println!("Simple Interest is {}", si);
+}
