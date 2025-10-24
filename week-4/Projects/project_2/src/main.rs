a1ae2bdfbc4124de5c5938cde90782a481d53741
matoeCOS101 <morgan.atoe@pau.edu.ng>
2025-10-24 03:12:39 +0100

    (My fifth commit)

---
+++ week-4/Projects/project_2/src/main.rs
@@ -0,0 +1,44 @@
+//Rust program to detemine the annual incentive
+//using the experience and age of an employee
+
+use std::io;
+
+fn main() {
+   println!("==Employee Annual Incentive Calculator==");
+
+//Experience 
+println!("Are you an experienced employee? (yes/no): ");
+let mut experience_input = String::new();
+io::stdin()
+.read_line(&mut experience_input)
+.expect("No input detected");
+
+let experience_input: bool = match experience_input.trim().to_lowercase().as_str(){
+    "yes" => true,
+    "no" => false,
+    _=> {
+    println!("Invalid input! Please enter 'yes' or 'no'.");
+    return; 
+ }
+};
+  if experience_input == false {
+    println!("Your incentive is N100_000 per month");
+ } else if experience_input == true {
+    println!("How old are you?");
+    let mut age = String::new();
+    io::stdin()
+    .read_line(&mut age)
+    .expect("No input detected");
+
+  if age >= 40.to_string() {
+    println!("Your incentive is N1_560_000 per month");
+  }
+  if age >= 30.to_string() && age < 40.to_string() {
+    println!("Your incentive is N1_480_000 per month");
+  }
+  if age < 28.to_string() && age < 30.to_string() {
+    println!("Your incentive is N1_300_000 per month");
+  }
+ }
+}
+
