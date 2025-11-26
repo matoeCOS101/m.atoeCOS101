5eedf0cd723298c7a77e15136f379ffef0abe6f6
matoeCOS101 <morgan.atoe@pau.edu.ng>
2025-11-26 12:53:43 +0100

    (Rust code to append data to a file)

---
+++ week-9/practice_4/target/debug/practice_4.d
@@ -0,0 +1,1 @@
+C:\Users\Morga\OneDrive\Desktop\Academics\Pan\ Atlantic\ University\COS\ 1O1\m.atoeCOS101\week-9\practice_4\target\debug\practice_4.exe: C:\Users\Morga\OneDrive\Desktop\Academics\Pan\ Atlantic\ University\COS\ 1O1\m.atoeCOS101\week-9\practice_4\src\main.rs
