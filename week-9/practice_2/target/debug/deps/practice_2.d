ff0c1b8d6c71a9943a03cc5a6b18df56cdd2e020
matoeCOS101 <morgan.atoe@pau.edu.ng>
2025-11-26 12:44:11 +0100

    (Rust code to read from a file)

---
+++ week-9/practice_2/target/debug/deps/practice_2.d
@@ -0,0 +1,5 @@
+C:\Users\Morga\OneDrive\Desktop\Academics\Pan Atlantic University\COS 1O1\m.atoeCOS101\week-9\practice_2\target\debug\deps\practice_2.d: src\main.rs
+
+C:\Users\Morga\OneDrive\Desktop\Academics\Pan Atlantic University\COS 1O1\m.atoeCOS101\week-9\practice_2\target\debug\deps\practice_2.exe: src\main.rs
+
+src\main.rs:
